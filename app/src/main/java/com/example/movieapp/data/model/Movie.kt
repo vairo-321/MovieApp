672e04daf1234e18048c54a9284a6f0cc17b3ac3
Esteban <diazesteban600@gmail.com>
2023-10-15 23:08:30 -0300

    componentes principales de ROOM agregados, falta implementacion de los mismos

--- app/src/main/java/com/example/movieapp/data/model/Movie.kt
+++ app/src/main/java/com/example/movieapp/data/model/Movie.kt
@@ -1,5 +1,9 @@
 package com.example.movieapp.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 data class Movie(
     val adult: Boolean = false,
     val backdrop_path: String = "",
@@ -14,6 +18,38 @@ data class Movie(
     val title: String = "",
     val video: Boolean = false,
     val vote_average: Double = -1.0,
+    val vote_count: Int = -1,
+)
+
+//Room
+
+@Entity
+data class MovieEntity(
+    @PrimaryKey
+    val id: Int = -1,
+    @ColumnInfo(name = "adult")
+    val adult: Boolean = false,
+    @ColumnInfo(name = "backdrop_path")
+    val backdrop_path: String = "",
+    @ColumnInfo(name = "original_language")
+    val original_language: String = "",
+    @ColumnInfo(name = "original_title")
+    val original_title: String = "",
+    @ColumnInfo(name = "overview")
+    val overview: String ="",
+    @ColumnInfo(name = "popularity")
+    val popularity: Double = -1.0,
+    @ColumnInfo(name = "poster_path")
+    val poster_path: String = "",
+    @ColumnInfo(name = "relese_date")
+    val relese_date: String = "",
+    @ColumnInfo(name = "title")
+    val title: String = "",
+    @ColumnInfo(name = "video")
+    val video: Boolean = false,
+    @ColumnInfo(name = "vote_average")
+    val vote_average: Double = -1.0,
+    @ColumnInfo(name = "vote_count")
     val vote_count: Int = -1,
 )
 
